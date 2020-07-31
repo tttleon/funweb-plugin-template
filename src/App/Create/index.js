7ad8e19a82d91a7e6b5a310de23a331b8fcce9fc
JACK-ZHANG-coming <z2422591495@163.com>
2020-07-31 15:52:48 +0800

    JSON格式化功能添加验证判断

--- src/App/Create/index.js
+++ src/App/Create/index.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext,useState } from 'react'
 import {
     Form,
     Input,
@@ -6,7 +6,7 @@ import {
     Radio,
     Select,
     message,
-    AutoComplete
+    AutoComplete,
 } from 'antd';
 import { SessionContext } from 'funweb-lib'
 import CreateApp from "./mutations/"
@@ -26,27 +26,48 @@ const validateMessages = {
     },
 };
 
-const CreateForm = props => { 
+var state = {
+    urlplus: '',//存放url的变量
+    jsonplus:'',//存放格式化后的json代码
+    jsonNow:'',//当前文本框显示的json代码
+};
 
-    var state = {
-        urlplus: '',//存放url的变量
-    };
-      
+const CreateForm = props => {      
+    //调试地址输入框
     function dealInput (event) {
         //console.log(event)    // 只有通过 event 才能获得输入框输入的值 
-        state.urlplus=event;
-        //console.log("state.urlplus:"+state.urlplus)
+        state.urlplus=event;        
        }
+       
+    //备注文本框
+    function onChangeTextArea(e){
+        console.log("onChangeTextArea:",e.target.value);        
+        state.jsonNow=e.target.value; 
+        setRemark(state.jsonNow)
+        console.log("state.jsonNow111:",state.jsonNow);
+    }
+    
+    //JSON格式转换
+    function onChangeJSON(e) {
+        console.log("state.jsonNow:",state.jsonNow);   
+        setRemark(JSON.stringify(JSON.parse(state.jsonNow),null,4));
+        state.jsonplus=JSON.stringify(JSON.parse(state.jsonNow),null,4);
+        console.log("1");
+        console.log('jsonNow:',state.jsonNow);
+        // console.log(`checked = ${e.target.checked}`);  //es6模板字符串
+    }    
 
     const session = useContext(SessionContext);
     props.title("创建应用信息");
     const { onCancel } = props;
 
+    const [remark, setRemark] = useState(''); //动态写值到remark
+
     const onFinish = values => {
         // console.log(state.urlplus)
         // console.log("values:"+ JSON.stringify(values)) //JSON.stringify(values) 转译Object
         // return
-        CreateApp.commit(session.environment, values.name, values.type, values.mode, state.urlplus, values.remark, (response, errors) => {
+        CreateApp.commit(session.environment, values.name, values.type, values.mode, state.urlplus, state.jsonplus, (response, errors) => {
             if (errors) {                
                 message.error(errors[0].message);
             } else {                
@@ -89,9 +110,13 @@ const CreateForm = props => {
             </Form.Item>
             <Form.Item name='version' label="版本">
                 <Input />
-            </Form.Item>
-            <Form.Item name='remark' label="备注">
-                <Input.TextArea />
+            </Form.Item>            
+            <Form.Item name='remark' label="备注">                
+                <Input.TextArea  value={remark} onChange={onChangeTextArea} ></Input.TextArea>
+                {/* <Checkbox onChange={onChangeJSON} >
+                    JSON格式化
+                </Checkbox> */}
+                <Button type="dashed" size="small"  onClick={onChangeJSON} >JSON格式化</Button>
             </Form.Item>
             <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
                 <Button type="primary" htmlType="submit">创建</Button>
